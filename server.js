9ae5a6765f07737abc862a2f0c7fbd297d6cc6c6
bert-bae <elbert.bae@gmail.com>
2018-10-28 22:31:40 +0000

    get express().use and .set

---
+++ server.js
@@ -0,0 +1,9 @@
+'use strict';
+var express = require('express');
+var routes = require('./routes/index.js');
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.set('view engine', 'ejs');
