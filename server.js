a6dee5c7b056b67042c174e0cfa1d5d12c616467
bert-bae <elbert.bae@gmail.com>
2018-10-28 22:47:04 +0000

    set-up routes folder with index.js

--- server.js
+++ server.js
@@ -5,5 +5,14 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 
+// setup our express application
 app.use('/public', express.static(process.cwd() + '/public'));
 app.set('view engine', 'ejs');
+
+// setup routes
+routes(app);
+
+// start our server
+app,list(port, function() {
+  console.log('Server listening on port ' + port + '...')
+});
